cab13c85586697c8efc1ec5a3c0743fc5614c5ca
Anthony Joyner <anthony.c.joyner@gmail.com>
2020-12-25 11:37:55 -0500

    Added video facial detection functionality.
    Signed-off-by: Anthony Joyner <anthony.c.joyner@gmail.com>

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,24 +1,39 @@
+from random import randrange
+
 import cv2
 
 # Load some pre-trained data on face frontals from open cv (haar cascade algorithm)
 trained_face_data = cv2.CascadeClassifier(
     'haarcascade_frontalface_default.xml')
 
-# Choose an image to detect faces ink
-img = cv2.imread('RDJ.jpeg')
+# Choose an image to detect faces link
+# img = cv2.imread('RDJ.jpeg')
 
-# Must convert to grey scale
-grey_scaled_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Detect faces
-face_coordinates = trained_face_data.detectMultiScale(grey_scaled_image)
+# capture video images for webcam (0 is the default webcam)
+webcam = cv2.VideoCapture(0)
 
-# Draw Rectangles around the faces
-for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+# capture video images for saved video (give the path of the video for any video)
+# video = cv2.VideoCapture('Screen Recording 2020-12-18 at 9.19.43 AM.mov')
 
-print(face_coordinates)
-cv2.imshow('Face Detector', grey_scaled_image)
-cv2.waitKey()
+while True:
+    # Read the current frame
+    successful_frame_read, frame = webcam.read()
 
-print("Code Completed")
+    # Must convert to grey scale
+    grey_scaled_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    face_coordinates = trained_face_data.detectMultiScale(grey_scaled_image)
+
+    # Draw Rectangles around the faces
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 10)
+
+    cv2.imshow('Face Detector', frame)
+    key = cv2.waitKey(1)
+
+    # Stop if Q key is pressed
+    if key == 81 or key == 113:
+        break
+    print("Code Completed")
